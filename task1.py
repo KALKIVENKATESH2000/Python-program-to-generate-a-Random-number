3346c3f9c5d9e01da8d11ba17f3d6d18d22a6001
Donga Kalki Venkatesh <105498992+KALKIVENKATESH2000@users.noreply.github.com>
2022-05-13 18:37:06 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+# Generating a list of random number using for loop.
+import random
+random_list = []
+
+for i in range(1):
+    n = random.randint(1, 50)
+    random_list.append(n)
+    print(random_list)
+
